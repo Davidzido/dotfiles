a0b13164dcd4e6e1c18586bd57884b869f2b4aa5
Davidzido <33006091+Davidzido@users.noreply.github.com>
2026-01-02 11:35:44 -0500

    Added color schemes to alacritty and changed the default one for wezterm.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,7 +16,7 @@ config.enable_tab_bar = false
 
 -- or, changing the font size and color scheme.
 config.font_size = 13
-config.color_scheme = 'Horizon Dark (base16)'
+config.color_scheme = 'Tokyo Night Storm'
 config.font = wezterm.font {
 	family = 'JetBrainsMono Nerd Font',
 	weight = 'Bold'
